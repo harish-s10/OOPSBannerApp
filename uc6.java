7f6b62a5199cb9dcb17329b87cc90ddbaeb1fbbd
Harish Sivakumar <harishssschool@gmail.com>
2026-03-10 13:28:37 +0530

    Create uc6.java

---
+++ uc6.java
@@ -0,0 +1,47 @@
+public class OOPSBannerApp {
+
+    // Static helper method for letter O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static helper method for letter P
+    public static String[] buildP() {
+        return new String[]{
+                "****** ",
+                "*    *",
+                "****** ",
+                "* ",
+                "* "
+        };
+    }
+
+    // Static helper method for letter S
+    public static String[] buildS() {
+        return new String[]{
+               "  ********** ",
+                " ** ",
+                "  ********** ",
+                "           **",
+                "  ********** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+
+        // Loop-based rendering
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+        }
+    }
+}
