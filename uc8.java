7bac0b7ff0cffb75a4ba1f4d531dcce5d960ef2d
Harish Sivakumar <harishssschool@gmail.com>
2026-03-10 13:47:17 +0530

    Create uc8.java

---
+++ uc8.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerApp {
+
+    // Centralized Pattern Storage
+    private static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "****** ",
+                "*    *",
+                "****** ",
+                "* ",
+                "* "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "* ",
+                " ***** ",
+                "      *",
+                " ***** "
+        });
+
+        return patternMap;
+    }
+
+    // Render Function
+    private static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        int height = 5;
+
+        for (int row = 0; row < height; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append(" ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        String word = "OOPS";
+
+        renderBanner(word, patternMap);
+    }
+}
